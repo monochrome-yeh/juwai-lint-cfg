a57fe7ca0e351c65daa9fb6dbdcd48d6e19c6ea4
Fabien Basmaison <fabien@juwai.com>
2015-12-01 21:51:52 +0800

    [JUW-1453] Fix async problem: - Add bluebird dependency. - Use promises to write files. - Fix too permissive path analysis. - Update .gitignore.

--- scripts/install.js
+++ scripts/install.js
@@ -1,13 +1,14 @@
+/* globals require */
 (function() {
     'use strict';
 
-    var console    = require( 'console' );
-    var fs         = require( 'fs' );
-    var module     = require( 'module' );
-    var process    = require( 'process' );
+    var Promise = require( 'bluebird' );
+    var console = require( 'console' );
+    var fs      = require( 'fs' );
+    var process = require( 'process' );
 
     var modulePath = process.cwd();
-    var files      = [
+    var files     = [
         '.csslintrc',
         '.editorconfig',
         '.jscsrc',
@@ -16,7 +17,9 @@
 
     // Open the standard input and ask a question.
     process.stdin.resume();
-    process.stdout.write( 'What is the name of the folder where the repository lives?\n' );
+    process.stdout.write(
+        'What is the name of the folder where the repository lives?\n'
+    );
 
     // When some answer is provided, try to copy the files.
     process.stdin.once( 'data', function( dest ) {
@@ -24,33 +27,51 @@
 
         // Make sure to have a clean user input.
         // Check if the string exists in the current path.
-        dest = dest.toString().trim();
+        dest = '/' + dest.toString().trim() + '/';
         destIndex = modulePath.indexOf( dest );
 
         if ( destIndex !== -1 ) {
-            destPath = modulePath.slice( 0, destIndex + dest.length + 1 );
-            copyFiles( destPath );
+            destPath = modulePath.slice( 0, destIndex + dest.length );
+            process.stdout.write(
+                'I found ' + destPath + '. I will copy the files now…\n'
+            );
+
+            // Exit current process only once all files are copied.
+            copyFiles( destPath ).then(function() {
+                console.log( 'All the files were copied successfully.' );
+                process.exit();
+            });
         } else {
             process.stdout.write(
                 'The folder “' + dest + '” doesn’t seem to exist in this ' +
                 'module’s path (' + modulePath + '), try again?\n'
             );
-        }
 
-        process.exit();
+            process.exit();
+        }
     });
 
     /**
      * Copy files to a specific folder.
+     * This function uses promises to deal with latency between read and write operations.
+     * Not doing so will lead to the creation of empty files.
      *
+     * @see  http://bluebirdjs.com/docs/api-reference.html
      * @param  {string} dest - Folder to copy the files to.
      */
     function copyFiles( dest ) {
-        files.forEach(function( file, index ) {
-            console.log( 'Copying ' + file + ' to ' + dest + file + '…' );
+        return Promise.all(
+            files.map(function( file, index ) {
+                var readFileAsync  = Promise.promisify( fs.readFile );
+                var writeFileAsync = Promise.promisify( fs.writeFile );
+                var fileRead       = readFileAsync( file );
 
-            // http://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
-            fs.createReadStream( file ).pipe( fs.createWriteStream( dest + file ) );
-        });
+                return fileRead.then( function( fileContent ) {
+                    console.log( 'Copying ' + file + ' to ' + dest + file + '…' );
+
+                    return writeFileAsync( dest + file, fileContent );
+                });
+            })
+        );
     }
 })();
